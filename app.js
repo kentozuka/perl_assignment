208a920920d1e5636fc60154fe7d475f2f9cbd08
kento <kentozuka22@gmail.com>
2020-07-31 15:10:41 +0900

    completed

--- app.js
+++ app.js
@@ -1,10 +1,14 @@
+const { exec } = require('child_process')
 const puppeteer = require('puppeteer')
+const libxmljs = require('libxmljs')
+const axios = require('axios')
 const fs = require('fs')
 
 const errMsg = '\n\n= = = = = = = = = = = = \nThis program runs on Node.js. Please install the latest version of node and run npm install.\n= = = = = = = = = = = = \n'
 const link = 'https://www.wsl.waseda.jp/syllabus/JAA101.php'
 const dept = '212004'
 const term = '1'
+const dir = './src'
 let counter = 1
 
 async function main () {
@@ -12,24 +16,23 @@ async function main () {
   const browser = await check()
   if (browser) {
     const page = await browser.newPage()
-    // accessing the top page -> searching
-    await page.goto(link, { waitUntil: 'domcontentloaded' })
-    await page.select('select[name="p_gakubu"]', dept)
-    await page.waitFor(2000)
-    await page.select('select[name="p_gakki"]', term)
-    await page.click('input[name="btnSubmit"]')
-    await page.waitFor(2000)
-    // extracting data
-    // eslint-disable-next-line
-    await page.evaluate(() => func_showchg('JAA103SubCon', '500')) // showing 500 results
-    await page.waitFor(2000)
-    const aa = await page.$$eval('td > a', li => li.map(x => x.getAttribute('onclick')))
-    const keys = aa.map(x => x.split('\'')[3])
+    const spring = await go(page, term)
+    const fall = await go(page, term + 1)
+    const keys = spring.concat(fall)
+    await browser.close()
     console.log('Done. Moving on to the next task.')
-    for (const key of keys) {
-      await getData(browser, key, keys.length)
+    if (!fs.existsSync(dir)) {
+      fs.mkdirSync(dir)
     }
-    console.log('All done. Run perl main.pl now.')
+    try {
+      for (const key of keys) {
+        await getData(key, keys.length)
+      }
+      console.log('All done. Running the perl code.')
+    } catch {
+      console.log('Access limit reached. Moving on.')
+    }
+    exec('perl main.pl', (error) => { if (error) return console.log(`error: ${error.message}`) })
   }
 }
 
@@ -42,21 +45,42 @@ async function check () {
   }
 }
 
-async function getData (browser, key, len) {
-  const url = `https://www.wsl.waseda.jp/syllabus/JAA104.php?pKey=${key}&pLng=en`
-  const path = `./src/${key}.txt`
-  const page = await browser.newPage(key)
-  await page.goto(url)
+async function go (page, term) {
+  // accessing the top page -> searching
+  await page.goto(link, { waitUntil: 'domcontentloaded' })
+  await page.select('select[name="p_gakubu"]', dept)
   await page.waitFor(2000)
-  const [data] = await page.$x('//th[text()="Course Outline"]/following-sibling::td')
-  const text = await (await data.getProperty('innerText')).jsonValue()
+  await page.select('select[name="p_gakki"]', term)
+  await page.click('input[name="btnSubmit"]')
+  await page.waitFor(2000)
+  const keys = await getKeys(page)
+  console.log(`Got ${keys.length} keys for term ${term}`)
+  return keys
+}
+
+async function getKeys (page) {
+  // extracting data
+  // eslint-disable-next-line
+  await page.evaluate(() => func_showchg('JAA103SubCon', '500')) // showing 500 results
+  await page.waitFor(2000)
+  const aa = await page.$$eval('td > a', li => li.map(x => x.getAttribute('onclick')))
+  const keys = aa.map(x => x.split('\'')[3])
+  return keys
+}
+
+async function getData (key, len) {
+  const url = `https://www.wsl.waseda.jp/syllabus/JAA104.php?pKey=${key}&pLng=en`
+  const path = `${dir}/${key}.txt`
+  const xml = await axios.get(url)
+  const xmlDoc = libxmljs.parseHtml(xml.data)
+  const co = xmlDoc.get('//th[text()="Course Outline"]/following-sibling::td')
+  const text = co.text()
   const lines = text.replace(/\n/g, '')
   fs.writeFile(path, lines, (err) => {
     if (err) console.log(err)
   })
   console.log(`Processing... ${counter} / ${len}`)
   counter += 1
-  await page.close()
 }
 
 main()
--- main.pl
+++ main.pl
@@ -1,116 +1,34 @@
 #!/usr/bin/perl
 
-use 5.010;
-use strict;
-use warnings;
+open (WORD_LIST, ">", 'raw_word_list.txt');
+open (RESULT, ">", "result.txt");
 
-use XML::LibXML;
+my $word_counter = 0;
+my %dictionary;
 
-my $testing_string = 'https://www.wsl.waseda.jp/syllabus/JAA104.php?pKey=210CM20100012020210CM2010021&pLng=en';
-my $testing_html = qx(curl $testing_string);
-
-my $dom = XML::LibXML->load_xml(string => $testing_html);
-
-foreach my $title ($dom->findnodes('//th[text()="Course Outline"]/following-sibling::td')) {
-    say $title->to_literal();
+print "Starting the perl.\n";
+opendir my $dir, "./src" or die "Cannot open directory: $!";
+my @files = readdir $dir;
+foreach my $file (@files) {
+    open(F, '<', "./src/$file") or die $!;
+    while(<F>){
+        my @spld = split(/ /, $_);
+        foreach (@spld) {
+            if ($_ =~ /[a-z]/i) {
+                my $word = $_ =~ s/,|\.|\?|\!|\(|\)|）|（|'|’|"|”|“//gr;
+                $word = lc $word;
+                $dictionary{$word}++;
+                $word_counter += 1;
+                print WORD_LIST "$word\n";
+            }
+        }
+    }
+    close(F);
 }
-
-# use XML::XPath;
-# use XML::XPath::XMLParser;
-
-# my $testing_string = 'https://www.wsl.waseda.jp/syllabus/JAA104.php?pKey=210CM20100012020210CM2010021&pLng=en';
-# my $testing_html = qx(curl $testing_string);
-
-# my $xp = XML::XPath->new(xml => $testing_html);
-# my $aa = $xp->getNodeText('//th[text()="Course Outline"]/following-sibling::td');
-
-# print "$aa";
-
-# # defining paths
-# my $path_one = './famaf_corpus/females_ask_males.xml';
-# my $path_two = './famaf_corpus/males_ask_females.xml';
-# my $res_one = "./outputs/fam.txt";
-# my $res_two = "./outputs/maf.txt";
-
-# # opening files
-# open (ONE, "< $path_one");
-# open (TWO, "< $path_two");
-
-# # defining result files
-# open (ONE_RES, ">", $res_one);
-# open (TWO_RES, ">", $res_two);
-
-# # defining counters
-# my $one_cnt = 1;
-# my $two_cnt = 1;
-# my $one_wrd = 0;
-# my $two_wrd = 0;
-# my $one_counting = 0;
-# my $two_counting = 0;
-
-# # array for keep track of threads id
-# my %one_obj;
-# my %two_obj;
-
-# # rading files
-# while (my $line = <ONE>) {
-#   chomp($line);
-#   # creating objects/dictionary/hashkeys or whatever this is from body tag
-#   if ($line =~ /<body>/) {
-#     $one_counting = 1;
-#   } elsif ($line =~ /<\/body>/ && $one_counting) {
-#     $one_counting = 0;
-#   } elsif ($one_counting) {
-#     # counting total word length
-#     my $len = length $line;
-#     if ($len > 1) {
-#     my @spld = split(/ /, $line);
-#     my $spld_len = @spld;
-#     $one_wrd += $spld_len;
-#     # doing something
-#     foreach (@spld) {
-#       # should regex [. , " ? ] but...
-#       $one_obj{$_} ++;
-#       }
-#     }
-    
-#   }
-#   $one_cnt += 1
-# }
-
-# foreach $word (sort { $one_obj{$b} <=> $one_obj{$a} } keys %one_obj) {
-#   my $rel_freq = ($one_obj{$word} / $one_cnt) * 100;
-#   print ONE_RES "$word: $rel_freq%\n";
-# }
-
-# # rading files
-# while (my $line = <TWO>) {
-#   chomp($line);
-#   # creating objects/dictionary/hashkeys or whatever this is from body tag
-#   if ($line =~ /<body>/) {
-#     $two_counting = 1;
-#   } elsif ($line =~ /<\/body>/ && $two_counting) {
-#     $two_counting = 0;
-#   } elsif ($two_counting) {
-#     # counting total word length
-#     my $len = length $line;
-#     if ($len > 1) {
-#     my @spld = split(/ /, $line);
-#     my $spld_len = @spld;
-#     $two_wrd += $spld_len;
-#     # doing something
-#     foreach (@spld) {
-#       # should regex [. , " ? ] but...
-#       $two_obj{$_} ++;
-#       }
-#     }
-    
-#   }
-#   $two_cnt += 1
-# }
-
-# foreach $word (sort { $two_obj{$b} <=> $two_obj{$a} } keys %two_obj) {
-#   my $rel_freq = ($two_obj{$word} / $two_cnt) * 100;
-#   print TWO_RES "$word: $rel_freq%\n";
-# }
-
+foreach my $str (sort { $dictionary{$b} <=> $dictionary{$a} } keys %dictionary) {
+    printf RESULT "%-31s %s\n", $str, $dictionary{$str};
+}
+print "Done. Found $word_counter words\n";
+close(WORD_LIST);
+close(RESULT);
+closedir $dir;
